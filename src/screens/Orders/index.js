26511afb7ca5cace6a80dcdfe24af4af0fa9a0cc
demircisena <demirci.sena@yahoo.com>
2022-11-18 18:40:32 +0300

    orders file updated!

--- src/screens/Orders/index.js
+++ src/screens/Orders/index.js
@@ -4,15 +4,10 @@ import styles from "./style";
 const Orders = () => {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <View style={styles.containerSearch}>
         <Text style={styles.text}>lets find your drink</Text>
         <TextInput style={styles.text_input} placeholder="Search for drinks" ></TextInput>
       </View>
-
-=======
-      <Text>Started Orders</Text>
->>>>>>> 460144b6dd68d4462f1dd76e895c54d82ad0d87c
     </View>
   );
 };
