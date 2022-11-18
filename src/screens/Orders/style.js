48c5f74743b51d04ae57e3a9b8ff1347b3cb2bc0
mustafadede <mustafa.dede.0016@gmail.com>
2022-11-18 19:53:24 +0300

    conflict

--- src/screens/Orders/style.js
+++ src/screens/Orders/style.js
@@ -4,40 +4,32 @@ import { theme1 } from "../../theme";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height:"100%",
+    height: "100%",
     backgroundColor: "#fff",
-    // alignItems: "center",
-   
-    
   },
-  containerSearch:{
-    position:"relative",
+  containerSearch: {
+    position: "relative",
 
-    width:"100%",
-    height:"35%",
-    backgroundColor:theme1.lightBrown,
-    borderBottomLeftRadius:24,
-    borderBottomRightRadius:24,
-
+    width: "100%",
+    height: "35%",
+    backgroundColor: theme1.lightBrown,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
   },
-  text:{
-    justifyContent:"center",
-   textAlign:"center",
-   
-    
+  text: {
+    justifyContent: "center",
+    textAlign: "center",
   },
   text_input: {
-  
     borderRadius: 24,
     width: "90%",
-    position:"absolute",
-    bottom:24,
+    position: "absolute",
+    bottom: 24,
     height: 42,
-    marginLeft:24,
-    marginRight:24,
-    
+    marginLeft: 24,
+    marginRight: 24,
+
     backgroundColor: theme1.gray,
-    
   },
 });
 
